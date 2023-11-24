aac017a9102a49f6eabe6df7e66e3de3c3e07a66
Stefan <110892250+StefanJesenko@users.noreply.github.com>
2023-11-24 10:56:25 +0100

    0.1.1
    OMYGOD

--- LA1302/script.js
+++ LA1302/script.js
@@ -1,42 +1,81 @@
-let createFormElementEarning = function(parent){
-
+const createFormElementEarning = function(parent){
     const formEarning = document.createElement("form");
+    formEarning.id = "formEarning";
 
     const inputEarning = document.createElement("input");
-    inputEarning.setAttribute("class", "inputEarnings")  
-    inputEarning.setAttribute("type", "number")
+    inputEarning.id = "inputEarning";
+    inputEarning.setAttribute("type", "number");
     
     const submitEarning = document.createElement("input");
-    submitEarning.setAttribute("class","inputEarnings")
-    submitEarning.setAttribute("type","submit");
-    submitEarning.setAttribute("value","save");
+    submitEarning.setAttribute("type", "submit");
+    submitEarning.setAttribute("value", "Save");
 
-    formEarning.appendChild(inputEarning);
+    const submitEarningText = document.createElement("input");
+    submitEarningText.id = "submitEarningText";
+    submitEarningText.setAttribute("type","string");
+    
+    formEarning.appendChild(submitEarningText);
+    formEarning.appendChild(inputEarning); 
     formEarning.appendChild(submitEarning);
-
+    
     parent.appendChild(formEarning);
-}
-let createFormElementDisbursements = function(parent){
+};
 
+const createFormElementDisbursements = function(parent){
     const formDisbursements = document.createElement("form");
+    formDisbursements.id = "formDisbursements";
 
-    const inputFormDisbursements = document.createElement("input");
-    inputFormDisbursements.setAttribute("class","inputFormDisbursements");
-    inputFormDisbursements.setAttribute("type", "number");
+    const inputDisbursements = document.createElement("input");
+    inputDisbursements.id = "inputDisbursements";
+    inputDisbursements.setAttribute("type", "number");
 
-    const submitFormDisbursements = document.createElement("input");
-    submitFormDisbursements.setAttribute("class","inputFormDisbursements")
-    submitFormDisbursements.setAttribute("type", "submit");
-    submitFormDisbursements.setAttribute("value","save");
+    const submitDisbursements = document.createElement("input");
+    submitDisbursements.setAttribute("type", "submit");
+    submitDisbursements.setAttribute("value", "Save");
 
-    formDisbursements.appendChild(inputFormDisbursements);
-    formDisbursements.appendChild(submitFormDisbursements);
+    const submitDisbursementsText = document.createElement("input");
+    submitDisbursementsText.id = "submitDisbursementsText";
+    submitDisbursementsText.setAttribute("type","string");
 
+    formDisbursements.appendChild(submitDisbursementsText);
+    formDisbursements.appendChild(inputDisbursements);
+    formDisbursements.appendChild(submitDisbursements);
     parent.appendChild(formDisbursements);
-}
+};
+
+const addToEarnList = function(e){
+    e.preventDefault();
+    const inputVal = document.getElementById("inputEarning").value;
+    const inputValText = document.getElementById("submitEarningText").value
+    if (inputVal) {
+        const earnList = document.getElementById("earnList"); 
+        const li_earn = document.createElement("li");
+        li_earn.textContent = inputValText + " " + inputVal;
+        earnList.appendChild(li_earn);
+        document.getElementById("submitEarningText", "inputEarning").value = '';
+    }
+   
+};
+
+const addToDisbursementList = function(e){
+    e.preventDefault();
+    const inputVal = document.getElementById("inputDisbursements").value;
+    const inputValText = document.getElementById("submitDisbursementsText").value;
+    if (inputVal) {
+        const disbursementList = document.getElementById("disbursementList");
+        const li_disburse = document.createElement("li");
+        li_disburse.textContent = inputValText + " " +inputVal;
+        disbursementList.appendChild(li_disburse);
+        document.getElementById("inputDisbursements", "submitDisbursementsText").value = '';
+    }
+};
 
 
-const form1 = document.getElementById("form1");
-const form2 = document.getElementById("form2");
-createFormElementEarning(form1);
-createFormElementDisbursements(form2);
+const formEarnsParent = document.getElementById("formEarns");
+const formDisbursementsParent = document.getElementById("formDisbursements");
+
+createFormElementEarning(formEarnsParent);
+createFormElementDisbursements(formDisbursementsParent);
+
+formEarnsParent.addEventListener("submit", addToEarnList);
+formDisbursementsParent.addEventListener("submit", addToDisbursementList);
