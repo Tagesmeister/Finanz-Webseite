3c512031e19c5a890ac9d1bc4f1201e3ca2b21e0
Tagesmeister <Pascal.Oestrich@outlook.de>
2023-12-01 08:31:35 +0100

    YES IT WORKS

--- LA1302/script.js
+++ LA1302/script.js
@@ -5,6 +5,7 @@ const createFormElementEarning = function(parent){
     const inputEarning = document.createElement("input");
     inputEarning.id = "inputEarning";
     inputEarning.setAttribute("type", "number");
+    inputEarning.setAttribute("step","any")
     
     const submitEarning = document.createElement("input");
     submitEarning.setAttribute("type", "submit");
@@ -45,37 +46,85 @@ const createFormElementDisbursements = function(parent){
 
 const addToEarnList = function(e){
     e.preventDefault();
-    const inputVal = document.getElementById("inputEarning").value;
-    const inputValText = document.getElementById("submitEarningText").value
-    if (inputVal) {
+    const inputEarning = document.getElementById("inputEarning");
+    const inputEarningText = document.getElementById("submitEarningText");
+
+    if (inputEarning.value) {
         const earnList = document.getElementById("earnList"); 
         const li_earn = document.createElement("li");
-        li_earn.textContent = inputValText + " " + inputVal;
+        li_earn.textContent = inputEarningText.value + " " + inputEarning.value;
         earnList.appendChild(li_earn);
-        document.getElementById("submitEarningText", "inputEarning").value = '';
+
+        inputEarning.value = '';
+        inputEarningText.value = '';
     }
    
 };
 
 const addToDisbursementList = function(e){
     e.preventDefault();
-    const inputVal = document.getElementById("inputDisbursements").value;
-    const inputValText = document.getElementById("submitDisbursementsText").value;
-    if (inputVal) {
+    const inputDisbursements = document.getElementById("inputDisbursements");
+    const inputDisbursementsText = document.getElementById("submitDisbursementsText");
+
+    if (inputDisbursements.value) {
         const disbursementList = document.getElementById("disbursementList");
         const li_disburse = document.createElement("li");
-        li_disburse.textContent = inputValText + " " +inputVal;
+        li_disburse.textContent = inputDisbursementsText.value + " " + inputDisbursements.value;
         disbursementList.appendChild(li_disburse);
-        document.getElementById("inputDisbursements", "submitDisbursementsText").value = '';
+
+        // Leere die Eingabefelder
+        inputDisbursements.value = '';
+        inputDisbursementsText.value = '';
     }
 };
 
 
+const calculateBudget = function(){
+const budget = calculateSumEarnings() - calculateSumDisbursement();
+
+outPutBudget(budget);
+}
+
+function calculateSumEarnings(){
+    const earnListItems = document.getElementById("earnList").children;
+    let sum = 0;
+    Array.from(earnListItems).forEach(function(item){
+        const value = parseFloat(item.textContent.split(" ")[1]);
+        if (!isNaN(value)) {
+            sum += value;
+        }
+    });
+    return sum;
+}
+
+function calculateSumDisbursement(){
+    const disbursementListItems = document.getElementById("disbursementList").children;
+    let sum = 0;
+    Array.from(disbursementListItems).forEach(function(item){
+        const value = parseFloat(item.textContent.split(" ")[1]);
+        if (!isNaN(value)) {
+            sum += value;
+        }
+    });
+    return sum;
+}
+function outPutBudget(budget){
+    document.getElementById('budgetDisplay').textContent = `Budget: $${budget.toFixed(2)}`;
+}
+
+
+
+
 const formEarnsParent = document.getElementById("formEarns");
 const formDisbursementsParent = document.getElementById("formDisbursements");
+
+const buttonToCalculate = document.getElementById("calculateBudgetButton");
 
 createFormElementEarning(formEarnsParent);
 createFormElementDisbursements(formDisbursementsParent);
 
 formEarnsParent.addEventListener("submit", addToEarnList);
 formDisbursementsParent.addEventListener("submit", addToDisbursementList);
+
+buttonToCalculate.addEventListener("click", calculateBudget);
+
